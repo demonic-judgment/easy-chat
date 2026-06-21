9ade0a94c344b7b93b25f930b3d4d05ebc16c9ff
demonic-judgment <demonic-judgment@users.noreply.github.com>
2026-06-21 09:47:13 +0000

    Revert commits 0f84db4..b1f8ea2
    This reverts 3 commit(s) from 0f84db4 to b1f8ea2.
    
    Reverted commits:
    b1f8ea2 feat: 规划悬浮按钮视频播放方案
    3096a4a Revert commits 0f84db4..0f84db4
    0f84db4 feat: 规划悬浮按钮视频播放方案
    
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -855,7 +855,6 @@ const handlePreviewRequest = async (content: string, images?: PendingImage[]) =>
   font-size: 16px;
   font-weight: 500;
   color: #333;
-  padding-right: 100px;
 }
 
 .header-avatar {
